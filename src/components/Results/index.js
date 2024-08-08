cb7f21c62f12a8e9fde14f65cc2546c3766013ae
koundinya21 <149105895+Koundinya21@users.noreply.github.com>
2024-08-08 13:50:43 +0530

    Update index.js

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,37 +1,88 @@
 import './index.css'
+// import {useContext} from 'react'
 import {Link} from 'react-router-dom'
 import Header from '../Header'
+// import DetailsContext from '../../ReactContext'
+// import TimeUp from '../TimeUp'
+
 // import ScoreContext from '../../ReactContext'
 
-const Results = ({location}) => {
-  const {score} = location.state
-  //   const {completedTime} = props
+// const Results = ({location}) => {
+//   const {score} = location.state
+//   const {completedTime} = props
+const Results = props => {
+  const {location} = props
+  const {score, isTimeRunning, elapsedTimeInSeconds} = location.state
+  console.log(elapsedTimeInSeconds)
+  console.log(isTimeRunning)
+  console.log(score)
 
-  return (
-    <>
+  const hours = Math.floor(elapsedTimeInSeconds / 3600)
+  const minutes = Math.floor((elapsedTimeInSeconds % 3600) / 60)
+  const seconds = Math.floor(elapsedTimeInSeconds % 60)
+  const disHr = hours > 9 ? hours : `0${hours}`
+  const disMin = minutes > 9 ? minutes : `0${minutes}`
+  const disSec = seconds > 9 ? seconds : `0${seconds}`
+  const formattedTime = `${disHr}:${disMin}:${disSec}`
+
+  const scoreValue = score >= 10 ? {score} : `0${score}`
+
+  return isTimeRunning ? (
+    <div>
       <Header />
+
       <div className="bg-container">
         <div className="component">
-          <div className="result-card-container">
-            <div className="bg-card">
-              <img
-                src="https://res.cloudinary.com/dowxofd2k/image/upload/v1711890610/Asset_2_1_c8uttm.png"
-                alt="submit"
-                className="img-submit"
-              />
-              <p className="para">Congrats! You completed the assessment.</p>
-              <p className="time-taken">Time Taken: 00</p>
-              <p className="score">Your Score: {score}</p>
-              <Link to="/assess/questions">
-                <button type="button" className="reattempt-btn">
-                  Reattempt
-                </button>
-              </Link>
-            </div>
-          </div>
+          <img
+            src="https://res.cloudinary.com/dowxofd2k/image/upload/v1711890610/Asset_2_1_c8uttm.png"
+            alt="submit"
+            className="img-submit"
+          />
+          <h1 className="para">Congrats! You completed the assessment</h1>
+          <p className="time-taken">
+            Time Taken:
+            <span className="formatted-time">{formattedTime}</span>
+          </p>
+          <p className="score">
+            Your Score:<span>{scoreValue}</span>
+          </p>
+          <Link to="/assessment">
+            <button type="button" className="reattempt-btn">
+              Reattempt
+            </button>
+          </Link>
         </div>
       </div>
-      {/* <div className="bg-container">
+    </div>
+  ) : (
+    <>
+      <Header />
+      <div className="container-time-up">
+        <div className="time-up-car">
+          <img
+            src="https://res.cloudinary.com/dowxofd2k/image/upload/v1711996764/calender_1_1_q3e1we.png"
+            alt="time up"
+            className="time-img-details"
+          />
+          <h1 className="time-up-heading">Time is up!</h1>
+          <p className="time-up-para">
+            You did not complete the assessment within the time
+          </p>
+          <p className="score">
+            Your Score:<span>{scoreValue}</span>
+          </p>
+          <Link to="/assessment">
+            <button type="button" className="Reattempt-btn">
+              Reattempt
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+/* <div className="bg-container">
         <div className="component">
           <div className="results-card_container">
             <img
@@ -50,10 +101,8 @@ const Results = ({location}) => {
             </Link>
           </div>
         </div>
-      </div> */}
-    </>
-  )
-}
+      </div> */
+
 //   <ScoreContext.Consumer>
 //     {value => {
 //       const {score} = value
